2cbb3d62086f650b7002cb5f3c0a98e5d73054e1
Mohit Rajak <mohitrajak022004@gmail.com>
2026-01-02 11:09:50 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,35 +3,76 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div
-      className="fixed top-0 w-full z-50 
-            bg-[#020617]/80 backdrop-blur-xl 
-            border-b border-white/10 h-[50px]"
-    >
-      <ul className="flex justify-between text-[#f8fafc] hover:text-[#38bdf8] transition">
-        <NavLink>
-          <h1>Portfolio</h1>
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+         <img className="w-[120px] h-auto object-contain" src="https://logowik.com/content/uploads/images/portfolio5182.logowik.com.webp" alt="" />
+
+        <ul className="flex items-center gap-12 text-gray-700 font-medium">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "hover:text-blue-600"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "hover:text-blue-600"
+              }
+            >
+              About
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/skills"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "hover:text-blue-600"
+              }
+            >
+              Skills
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "hover:text-blue-600"
+              }
+            >
+              Projects
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "hover:text-blue-600"
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+
+        <NavLink
+          to="/resume"
+          className="px-5 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-700 transition"
+        >
+          Resume
         </NavLink>
-        <NavLink>
-          <p>Home</p>
-        </NavLink>
-        <NavLink>
-          <p>About</p>
-        </NavLink>
-        <NavLink>
-          <p>Skills</p>
-        </NavLink>
-        <NavLink>
-          <p>Projects</p>
-        </NavLink>
-        <NavLink>
-          <p>Resume</p>
-        </NavLink>
-        <NavLink>
-          <p>Contact</p>
-        </NavLink>
-      </ul>
-    </div>
+
+      </div>
+    </nav>
   );
 };
 
