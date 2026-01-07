3bc48c81572adc8da1304a8bbe5f41d3c4f593dd
Mohit Rajak <mohitrajak022004@gmail.com>
2026-01-07 12:02:48 +0530

    Day 2 of ProjectCard

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -8,7 +8,7 @@ export const assets = {
 export const projects = [
     {
     image : [zync],
-    name : "E-commerse",
+    name : "E-commerse Fashion Website",
     description : "Almost every advanced algorithm internally depends on the idea of ordering data. If you understand these basics clearly, learning Merge Sort, Quick Sort, Heap Sort, Radix Sort becomes much easier.",
     }
 ]
