802367d9f9ed4c54697a578ecc828e98233a6eb5
Mohit Rajak <mohitrajak022004@gmail.com>
2026-01-08 18:13:21 +0530

    Updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,37 +1,99 @@
-import zync from './zync.png';
-
+import zync from "./zync.png";
 
 export const assets = {
-    zync,
-}
+  zync,
+};
 
 // assets.js
 export const projects = [
   {
     id: 1,
-    name: "Fashion Store",
-    description: "Modern fashion e-commerce website built using MERN stack.",
+    name: "E-Commerce Website",
+    description:
+      "A full-stack E-Commerce web application built using the MERN Stack (MongoDB, Express.js, React.js, Node.js) and styled with Tailwind CSS. The platform supports user authentication, product browsing, orders, and an Admin panel where only admins can upload and manage products.",
     image: [zync],
     liveLink: "https://fashion-frontend-two.vercel.app/",
-    githubLink: "https://github.com/mohitrajak/fashion-store"
+    githubLink: "https://github.com/mohitrajak/fashion-store",
   },
   {
     id: 2,
-    name: "Stock Sphere",
-    description: "Stock market tracking & analysis web app.",
-    image: ["/images/stock.png"],
-    liveLink: "https://stocksphere.vercel.app/",
-    githubLink: "https://github.com/mohitrajak/stock-sphere"
+    name: "Chat Application",
+    description:
+      "A real-time Chat application built using the MERN stack (MongoDB, Express, React, Node.js) and styled with Tailwind CSS. It supports user authentication, group chats, and instant messaging with real-time updates via Socket.IO. The application is fully responsive and provides smooth communication with features like notifications and state management using Redux.",
+    image: [""],
+    liveLink: "https://chat-frontend-zeta-ecru.vercel.app/signin",
+    githubLink: "https://github.com/Mohit-Rajak/chat-frontend",
   },
   {
     id: 3,
-    name: "Chat App",
-    description: "Real-time chat application using Socket.io.",
+    name: "Food App",
+    description:
+      "Welcome to Food App, a cutting-edge food delivery app designed to revolutionize your dining experience. Get ready to enjoy seamless food ordering with a user-friendly interface and personalized features.",
     image: ["/images/chat.png"],
     liveLink: "https://chatapp.vercel.app/",
-    githubLink: "https://github.com/mohitrajak/chat-app"
-  }
+    githubLink: "https://github.com/Mohit-Rajak/FoodApp",
+  },
+  {
+    id: 4,
+    name: "My PortFolio",
+    description:
+      "This is my personal portfolio website showcasing my skills, projects, and experience. It serves as a digital resume and a way for potential employers or collaborators to learn more about me and my work.",
+    image: ["/images/chat.png"],
+    liveLink: "https://myportfolio-three-olive.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/My_Portfolio",
+  },
+  {
+    id: 5,
+    name: "Airbnb Website",
+    description:
+      "Excited to share my journey of developing a feature-rich full-stack web application airbnb. The project is built using MongoDB, Express.js, and Node.js.",
+    image: ["/images/chat.png"],
+    liveLink: "https://chatapp.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/AirbnbWebsite",
+  },
+  {
+    id: 6,
+    name: "Swiggy CLone",
+    description:
+      "Welcome to Food App, a cutting-edge food delivery app designed to revolutionize your dining experience. Get ready to enjoy seamless food ordering with a user-friendly interface and personalized features.",
+    image: ["/images/chat.png"],
+    liveLink: "https://swiggy-clone-ashen-eta.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/Swiggy_Clone",
+  },
+  {
+    id: 7,
+    name: "Stock Spehre",
+    description:
+      "StockSphere is a stock tracking and portfolio management web application built using the MERN stack (MongoDB, Express, React, Node.js) and styled with BootStrap CSS. The platform enables users to monitor live market data, manage stock portfolios, and utilize a custom dashboard interface with secure user authentication. It includes admin tools, responsive UI, and supports scalable backend APIs.",
+    image: ["/images/chat.png"],
+    liveLink: "https://chatapp.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/StockSphere",
+  },
+  {
+    id: 8,
+    name: "AI Code Reviewers",
+    description:
+      "The AI-Powered Code Reviewer is a MERN stack-based application designed to help developers review their code efficiently. It uses Google's Gemini AI to analyze code and provide detailed feedback on code quality, best practices, performance, and more. This tool is perfect for developers who want to improve their code quality and adhere to industry standards.",
+    image: ["/images/chat.png"],
+    liveLink: "https://chatapp.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/AI_Code_Reviewer",
+  },
+  {
+    id: 9,
+    name: "Pizza Landing Page",
+    description:
+      "I'm thrilled to share my latest project: a dynamic and engaging pizza website built using HTML, CSS, and JavaScriptThis project aims to deliver an exceptional user experience for pizza enthusiasts. From browsing the menu to placing an order, every interaction has been thoughtfully designed to be smooth and intuitive. The website not only showcases the delicious variety of pizzas available but also provides a visually appealing and responsive interface for users.",
+    image: ["/images/chat.png"],
+    liveLink: "https://pizza-landing-page-phi.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/Pizza-Landing-page",
+  },
+  {
+    id: 10,
+    name: "Education Website",
+    description:
+      "Welcome to the Responsive Education Website project! This repository showcases a modern, interactive, and fully responsive education platform built with HTML, CSS, and JavaScript. Our goal is to provide an intuitive and engaging online learning experience that adapts seamlessly across all devices, from desktops to smartphones.",
+    image: ["/images/chat.png"],
+    liveLink: "https://education-website-ten-rust.vercel.app/",
+    githubLink: "https://github.com/Mohit-Rajak/EducationWebsite",
+  },
 ];
-
-
-
